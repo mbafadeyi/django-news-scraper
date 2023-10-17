ab9650a4153a8b79300bbb52f2b47074c9d25090
M.B.A. Fadeyi <fadeyibajide@gmail.com>
2023-10-17 22:57:49 +0100

    Task Status

--- core/tasks.py
+++ core/tasks.py
@@ -2,9 +2,16 @@ from celery import shared_task
 
 from .scrapers import scrape
 
+URL = "https://dev.to/search?q=django"
+
 
 @shared_task
 def scrape_dev_to():
-    URL = "https://dev.to/search?q=django"
+    scrape(URL)
+    return
+
+
+@shared_task
+def scrape_async():
     scrape(URL)
     return
--- core/views.py
+++ core/views.py
@@ -1,12 +1,36 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.shortcuts import reverse
 from django.views import generic
 
+from .forms import ScrapeForm
 from .models import NewsItem, ScrapeRecord
+from .tasks import scrape_async
 
 
-class ScrapeRecordListView(generic.ListView):
+class ScrapeRecordListView(generic.FormView):
     template_name = "scrape_history.html"
-    paginate_by = 10
-    model = ScrapeRecord
+    form_class = ScrapeForm
+
+    def get_success_url(self):
+        return reverse("scrape-history")
+
+    def form_valid(self, form):
+        scrape_async.delay()
+        return super(ScrapeRecordListView, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(ScrapeRecordListView, self).get_context_data(**kwargs)
+        page = self.request.GET.get("page", 1)
+        qs = ScrapeRecord.objects.all()
+        paginator = Paginator(qs, 10)
+        try:
+            qs = paginator.page(page)
+        except PageNotAnInteger:
+            qs = paginator.page(1)
+        except EmptyPage:
+            qs = paginator.page(paginator.num_pages)
+        context.update({"object_list": qs})
+        return context
 
 
 class NewsItemListView(generic.ListView):
